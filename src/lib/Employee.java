d6f963df11736aa7a110012b9bd8779b59d2aebe
Foxxie1440 <90520474+ZhafranHafizh@users.noreply.github.com>
2024-04-20 20:02:07 +0700

    Make replacement obj
    membuat objek Nationality untuk menggantikan isForeigner, serta objek LocalDate untuk dateJoined.

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -8,44 +8,37 @@ import java.util.List;
 public class Employee {
 
 	private String employeeId;
-	private String firstName;
-	private String lastName;
-	private String idNumber;
-	private String address;
-	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
-	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
+    private LocalDate dateJoined;
+    private Nationality nationality;
+    private Gender gender;
+
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int annualDeductible;
+
+    private String spouseName;
+    private String spouseIdNumber;
+
+    private List<Child> children;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
-	}
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address,
+                    LocalDate dateJoined, Nationality nationality, Gender gender) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+        this.dateJoined = dateJoined;
+        this.nationality = nationality;
+        this.gender = gender;
+
+        children = new LinkedList<>();
+    }
 	
 	/**
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
